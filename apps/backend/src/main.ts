dcc9f6520e62341de15cc240a2daa20d771b37bc
Chris Jarman <chris.j.jarman@gmail.com>
2024-10-08 10:20:46 +0100

    fix(backend): enable cors indiscriminately
    Once we have domain origins under control we should tighten this up.

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -10,6 +10,10 @@ async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
+    {
+      // FIXME(cj): Once we have domain origins under control we should tighten this up.
+      cors: true,
+    },
   );
 
   await app.listen(3000, '0.0.0.0');
